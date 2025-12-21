2a8b56d3d32d99393b64d24c47fda0ea7bc2c71d
Philip Levy <pglevy@users.noreply.github.com>
2025-12-21 17:47:37 -0500

    add base path to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Switch } from "wouter"
+import { Route, Switch, Router } from "wouter"
 import Home from "./Home"
 import ChatDemo from "./ChatDemo"
 import CoreComponents from "./CoreComponents"
@@ -6,14 +6,19 @@ import { Toaster } from "sonner"
 import { ThemeProvider } from "./components/shared/theme-provider"
 
 function App() {
+  // Get base path from Vite's import.meta.env.BASE_URL
+  const base = import.meta.env.BASE_URL.replace(/\/$/, '')
+
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/chat" component={ChatDemo} />
-        <Route path="/core" component={CoreComponents} />
-        <Route>404: Not Found</Route>
-      </Switch>
+      <Router base={base}>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/chat" component={ChatDemo} />
+          <Route path="/core" component={CoreComponents} />
+          <Route>404: Not Found</Route>
+        </Switch>
+      </Router>
       <Toaster />
     </ThemeProvider>
   )
